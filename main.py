e03f2a18d3a3a15bfb258159be4de5f082cd70dc
DarkMatter-999 <darkmatter999official@gmail.com>
2024-12-10 22:23:34 +0530

    Added systray icon

--- main.py
+++ main.py
@@ -1,28 +1,68 @@
 import schedule
 import time
 
+from PyQt6.QtWidgets import QApplication, QWidget, QSystemTrayIcon, QMenu
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon, QAction
+
 from config import *
 from breaks import *
+from tray import SystemTrayIcon
+
+class PyLeoApp:
+    def __init__(self):
+        self.event_exit_app = None
+        """Called when the user clicks the Exit option from the system tray context menu"""
+
+        self._app = QApplication([])
+        self._app.setQuitOnLastWindowClosed(False)
+        self._widget = QWidget()
+        self._icon = SystemTrayIcon(QIcon("assets/leotrayicon.png"), self._widget)
+        self._icon.event_exit_click = self.on_exit_click
+
+        self._icon.event_start_break_click = self.on_start_break_click
+        self._icon.event_start_long_break_click = self.on_start_long_break_click
+
+        self._main_window = None
+        self._full_window = None
+
+        BREAK_INTERVAL = 5
+        self._short_timer = QTimer(self._app)
+        self._short_timer.timeout.connect(self.schedule_next_break)
+        self._short_timer.start(BREAK_INTERVAL * 1000)
+
+        self._long_timer = QTimer(self._app)
+        self._long_timer.timeout.connect(self.schedule_next_long_break)
+        self._long_timer.start(15 * 1000)
 
 
-def mainWindow():
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec()
+    def run(self):
+        self._app.exec()
 
-def fullWindow():
-    app = QApplication([])
-    window = FullWindow()
-    window.show()
-    app.exec()
+    def fire_exit_app(self):
+        if self.event_exit_app:
+            self.event_exit_app(self)
 
+    def on_exit_click(self, sender):
+        self.fire_exit_app() 
 
-if __name__ == "__main__":
+    def on_start_break_click(self, sender):
+        if self._main_window is None:
+            self._main_window = MainWindow()
+        self._main_window.show()
 
-    schedule.every(SHORT_BREAK).minutes.do(mainWindow)
-    schedule.every(LONG_BREAK).minutes.do(fullWindow)
+    def on_start_long_break_click(self, sender):
+        if self._full_window is None:
+            self._full_window = FullWindow()
+        self._full_window.show()
 
-    while True:
-        schedule.run_pending()
-        time.sleep(0.1)
+    def schedule_next_break(self):
+        self.on_start_break_click(self)
+
+    def schedule_next_long_break(self):
+        self.on_start_long_break_click(self)
+
+if __name__ == '__main__':
+    app = PyLeoApp()
+    app.run()
+
---
+++ tray.py
@@ -0,0 +1,60 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QSystemTrayIcon, QMenu
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon, QAction
+
+from breaks import *
+
+class SystemTrayIcon(QSystemTrayIcon):
+    def __init__(self, icon, parent=None):
+        super().__init__(icon, parent)
+        self.event_play_click = None
+        self.event_pause_click = None
+        self.event_exit_click = None
+
+        menu = QMenu(parent)
+
+        self._start_break_action = QAction("Start Short break", parent)
+        self._start_break_action.triggered.connect(self.on_start_break_click)
+        menu.addAction(self._start_break_action)
+
+        self._start_long_break_action = QAction("Start Long break", parent)
+        self._start_long_break_action.triggered.connect(self.on_start_long_break_click)
+        menu.addAction(self._start_long_break_action)
+
+        self._exit_action = QAction("Exit", parent)
+        self._exit_action.triggered.connect(self.on_exit_click)
+        menu.addAction(self._exit_action)
+
+        self.setContextMenu(menu)
+
+        # self.activated.connect(self.on_icon_click)
+        self.show()
+
+    def on_start_break_click(self):
+        self.fire_start_break_click()
+
+    def fire_start_break_click(self):
+        if self.event_start_break_click:
+            self.event_start_break_click(self)
+
+    def on_start_long_break_click(self):
+        self.fire_long_break_click()
+
+    def fire_long_break_click(self):
+        if self.event_start_long_break_click:
+            self.event_start_long_break_click(self)
+
+    def on_exit_click(self):
+        self.fire_exit_click()
+        print("Exiting")
+        QApplication.quit()
+
+    # def on_icon_click(self, reason):
+    #     if reason == QSystemTrayIcon.ActivationReason.Trigger:
+    #         new_icon = QIcon("assets/leotrayicon.png")
+    #         self.setIcon(new_icon)
+    
+    def fire_exit_click(self):
+        if self.event_exit_click:
+            self.event_exit_click(self)
+
